6957ab0251fd20868075f6b19d6d5d226ab0cc0a
Kamalia\kamalia <kamalia.farhana@visi.com.my>
2024-12-14 11:32:01 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\Companies\Index;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\AuthenticatedSessionController;
 
